c323bd80b1dd5d39e849f7ba905f5962e47379a1
Simon Slamka <simon.slamka@ongakken.com>
2024-03-08 08:48:58 +0100

    added sadness recognition and plotting within EDA

--- eda.py
+++ eda.py
@@ -2,10 +2,33 @@ import pandas as pd
 import json
 from matplotlib import pyplot as plt
 import mplcyberpunk
+from m3u_parser import M3uParser
+
+m = "data/sad.m3u"
+parser = M3uParser()
+parser.parse_m3u(m)
+tracks = parser.get_list()
+sad = []
+for t in tracks:
+    sad.append(t["name"])
+m = "data/intro.m3u"
+parser = M3uParser()
+parser.parse_m3u(m)
+tracks = parser.get_list()
+intro = []
+for t in tracks:
+    intro.append(t["name"])
+
+sadTracks = pd.DataFrame(sad, columns=["track"])
+introTracks = pd.DataFrame(intro, columns=["track"])
+# the artist name is the first part of the track name, before the first hyphen
+negArtists = sadTracks["track"].str.split(" - ").str[0]
+negArtists = negArtists[~negArtists.isin(["Imagine Dragons", "The Chainsmokers", "OneRepublic", "Sandro Cavazza", "Kygo", "Avicii"])]
 
 j = json.load(open("data/wakatime-smdexec000protonmail.ch-70f280d92d7342e5894da0d0f2acbd75.json"))
 # now, read the csv from Last.FM and display head
 c = pd.read_csv("data/lastfm.csv")
+
 print(c.head())
 # the first col is the artist, the third is the track, and the last is the date + time
 print(f"Artist: {c.iloc[0, 0]}, Track: {c.iloc[0, 2]}, Date: {c.iloc[0, 3]}")
@@ -68,13 +91,48 @@ print("Most active hours:", heartbeats.resample("h", on="created_at")["dur"].sum
 listenedTracksHourlySums = c.resample("h", on="date").size()
 listenedTracksDailySums = c.resample("D", on="date").size()
 
+# get count of when listened to "Falling in Reverse" over time since start of data
+fir = c[c.iloc[:, 0] == "Falling in Reverse"]
+# now, count occurences of `negArtists` in `c  over time
+neg = c[c.iloc[:, 0].isin(negArtists)]
+print(f"neg head: {neg.head()}")
+
+# get the top 3 negative tracks and count of listens
+print(f"Top 3 negative tracks: {neg.iloc[:, 2].value_counts().head(3)}")
+
+firDailySums = fir.resample("D", on="date").size()
+negDailySums = neg.resample("D", on="date").size()
+# outer join the two series
+sadSums = firDailySums.combine(negDailySums, max, fill_value=0)
+
+sadSumsMovingAvg = sadSums.rolling(7).mean()
+
 plt.style.use("cyberpunk")
 plt.figure(figsize=(20, 5))
-# plt.plot(listenedTracksDailySums.index, listenedTracksDailySums, color="red")
-plt.plot(dailiyHours.index, dailiyHours, color="orange")
-plt.plot(dailiyHoursMovingAvg.index, dailiyHoursMovingAvg, color="black", linewidth=3)
-plt.title("Daily Active WakaTime Time")
+
+plt.plot(sadSumsMovingAvg.index, sadSumsMovingAvg, color="red", linewidth=3)
+plt.title("m3u playlist-based negative tracks listens (7-day MA)")
 plt.xlabel("Date")
-plt.ylabel("Active Time (hours)")
+plt.ylabel("Listens (7-day MA)")
+
+# print every other month on the x-axis. don't print days, since there's too many
+plt.xticks(sadSumsMovingAvg.index[::90], [date.strftime('%b %Y') for date in sadSumsMovingAvg.index[::90]], rotation=45)
+
+# Highlight the sections as per the comment, using the total data range to get the ranges
+plt.axhspan(sadSumsMovingAvg.min(), sadSumsMovingAvg.quantile(0.25), color='green', alpha=0.3)
+plt.axhspan(sadSumsMovingAvg.quantile(0.25), sadSumsMovingAvg.quantile(0.5), color='yellow', alpha=0.3)
+plt.axhspan(sadSumsMovingAvg.quantile(0.5), sadSumsMovingAvg.quantile(0.75), color='orange', alpha=0.3)
+plt.axhspan(sadSumsMovingAvg.quantile(0.75), sadSumsMovingAvg.max(), color='red', alpha=0.3)
+
+plt.legend(["7-day MA", "Happy (25th-50th quant)", "Mostly fine (50th-75th quant)", "Sad (75th-100th quant)", "Extremely sad (100th quant)"])
+
 plt.tight_layout()
+plt.savefig("sad_7day_ma.png")
 plt.show()
+
+# plt.plot(listenedTracksDailySums.index, listenedTracksDailySums, color="red")
+# plt.plot(dailiyHours.index, dailiyHours, color="orange")
+# plt.plot(dailiyHoursMovingAvg.index, dailiyHoursMovingAvg, color="black", linewidth=3)
+# plt.title("Daily Active WakaTime Time")
+# plt.xlabel("Date")
+# plt.ylabel("Active Time (hours)")
